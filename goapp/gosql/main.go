1feb0b5a2d4f10e6e0286491b61ba0fdff1a2b59
17t2037h <17t2037h@shinshu-u.ac.jp>
2021-05-18 20:41:54 +0900

    http://go-database-sql.org/retrieving.html

--- goapp/gosql/main.go
+++ goapp/gosql/main.go
@@ -11,9 +11,35 @@ import (
 
 func main() {
 	db, err := sql.Open("mysql",
-		"root:test@tcp(mysql:3306)/training_DB")
+		"root:test@tcp(mysql:3306)/training_DB") //DB_HOST : mysql (not localhost)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	var (
+		id   int
+		name string
+	)
+
+	stmt, err := db.Prepare("select id, name from items where id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query(1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, name)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
